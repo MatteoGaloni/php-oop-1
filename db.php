55a70da3d26521d6160ab64fd5244dc99b2d6f71
MatteoGaloni <mtgaloni@gmail.com>
2023-06-20 22:09:26 +0200

    add style

--- db.php
+++ db.php
@@ -3,6 +3,9 @@ include './Models/movie.php';
 include './Models/actor.php';
 
 $blackMirror = new Movie("Black-Mirror", "EN", ["Science fiction, Dystopia, Anthology Speculative fiction"], new Actor("Josh", "Hartnett", "Non Protagonista"));
-$leFateIgnoranti = new Movie("Le fate Ignoranti", "IT", ["Commedia", "Drammatico"], new Actor("Stefano", "Accorsi", "Protagonista"));
+$leFateIgnoranti = new Movie("Le fate Ignoranti", "IT", ["Commedia", "Drammatico", "Romantico", "Italiano"], new Actor("Stefano", "Accorsi", "Protagonista"));
+
+$blackMirror->img = "https://upload.wikimedia.org/wikipedia/en/2/24/BlackMirrorTitleCard.jpg";
+$leFateIgnoranti->img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR84CBsEsMhPZ3BN0mrjVHI_h_9KA--lEDesR1do640&s";
 
 $movies = [$blackMirror, $leFateIgnoranti];
--- index.php
+++ index.php
@@ -13,23 +13,35 @@ include './db.php';
     <title>PHP-OOP</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-6">
-                <?php foreach ($movies as $movie) {  ?>
-                    <div class="card">
+            <?php foreach ($movies as $movie) {  ?>
+                <div class="col-6">
+                    <div class="card" style="width: 18rem;">
                         <img class="card-img-top" src="<?php echo $movie->img ?>" alt="Card image cap" />
                         <div class="card-body">
                             <h2 class="card-title"> <?php echo $movie->title ?></h2>
-                            <p class="card-title"> <?php echo $movie->overview ?></p>
-                            <h2> <?php $movie->getFlag() ?></h2>
+                            <p class="card-text"> <?php echo $movie->overview ?></p>
+                            <h4>Starring</h4>
+                            <span><?php echo $movie->actor->name ?></span>
+                            <span> <?php echo $movie->actor->surname ?></span>
+                            <h4>Role</h4>
+                            <span><?php echo $movie->actor->role ?></span>
+                            <h4>Genre</h4>
+                            <?php foreach ($movie->category as $item) { ?>
+                                <span>
+                                    <?php echo $item ?>
+                                </span>
+                            <?php  } ?>
+                            <h4> <?php $movie->getFlag() ?></h4>
                         </div>
                     </div>
-                <?php  } ?>
-            </div>
+                </div>
+            <?php  } ?>
         </div>
     </div>
 </body>
